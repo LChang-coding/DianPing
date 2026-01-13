4ad9ff5a25a7e4ded98b4f6b1703d25e390fbbed
LChang-coding <jwjwcwcw18714610648@gmail.com>
2026-01-13 15:57:11 +0800

    实现发送验证码和验证码的验证功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -14,7 +23,56 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        //校验手机号
+        if(RegexUtils.isPhoneInvalid(phone)){
+            //手机号格式不正确
+            return Result.fail("手机号格式不正确");
+        }
+        //校验验证码
+        String cacheCode=session.getAttribute("code").toString();//正确的验证码
+        String code=loginForm.getCode();//用户输入的验证码
+        if(cacheCode==null||!cacheCode.equals(code)){
+            return Result.fail("验证码错误");
+        }
+        //根据手机号查数据库
+        User user=query().eq("phone",phone).one();
+        if(user==null){
+            //注册用户 ->创建新用户
+            user=createUserWithPhone(phone);
+        }
+        session.setAttribute("user",user);//设置到session中
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        //创建用户
+        User user=new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        save(user);//保存用户到数据库
+        return user;
+    }
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号
+        if(RegexUtils.isPhoneInvalid(phone)){
+            //手机号格式不正确
+            return Result.fail("手机号格式不正确");
+        }
+        //生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        //保存验证码到session
+        session.setAttribute("code", code);
+        log.debug("发送短信验证码成功，验证码：{}",code);
+
+        return Result.ok();
+    }
 }
